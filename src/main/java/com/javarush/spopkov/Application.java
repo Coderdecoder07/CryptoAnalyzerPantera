916fd9c1264ed22deaa34629f4548dc0164a4920
Coderdecoder07 <coderdecoder07@gmail.com>
2025-08-19 16:14:29 +0300

    CaesarCipherRunner + Application fix

--- src/main/java/com/javarush/spopkov/Application.java
+++ src/main/java/com/javarush/spopkov/Application.java
@@ -1,4 +1,11 @@
 package com.javarush.spopkov;
 
 public class Application {
+    public Application(String[] args) {
+
+    }
+
+    public Result run() {
+        return null;
+    }
 }
--- src/main/java/com/javarush/spopkov/CaesarCipherRunner.java
+++ src/main/java/com/javarush/spopkov/CaesarCipherRunner.java
@@ -2,10 +2,19 @@ package com.javarush.spopkov;
 
 public class CaesarCipherRunner {
     public static void main(String[] args) {
-        String RusAlphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-        String EngAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String numbers = "0123456789";
-        String symbols = "~!@#$%^&*()_+{}|:<>?-=[];',./`";
-        String alphabet = RusAlphabet + EngAlphabet + RusAlphabet.toLowerCase() + EngAlphabet.toLowerCase() + numbers + symbols;
+
+        Application app = new Application(args);
+        Result result = app.run();
+        System.out.println(result);
+
+
+
+
     }
 }
+
+
+
+        /*int key = 17;
+        String text = ".....";
+        String result =".....";*/
