7e9048b259fd152a44372ada8f120ec21e46640a
Coderdecoder07 <coderdecoder07@gmail.com>
2025-08-25 18:55:37 +0300

    Add Constants and Actions classes

---
+++ src/main/java/com/javarush/spopkov/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.javarush.spopkov.constants;
+
+public class Constants {
+    public static final String RusAlphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    public static final String EngAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    /*private static final String numbers = "0123456789";
+    private static final String symbols = "~!@#$%^&*()_+{}|:<>?-=[];',./`";*/
+}
