5099f4c0421209735c9a56db04343461d65efd77
Coderdecoder07 <coderdecoder07@gmail.com>
2025-08-25 20:48:07 +0300

    Constants fix

--- src/main/java/com/javarush/spopkov/constants/Constants.java
+++ src/main/java/com/javarush/spopkov/constants/Constants.java
@@ -1,8 +1,8 @@
 package com.javarush.spopkov.constants;
 
 public class Constants {
-    public static final String RusAlphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-    public static final String EngAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    /*private static final String numbers = "0123456789";
-    private static final String symbols = "~!@#$%^&*()_+{}|:<>?-=[];',./`";*/
+    public static final String RUS_ALPHABET = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    public static final String ENG_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    /*private static final String NUMBERS = "0123456789";
+    private static final String SYMBOLS = "~!@#$%^&*()_+{}|:<>?-=[];',./`";*/
 }
